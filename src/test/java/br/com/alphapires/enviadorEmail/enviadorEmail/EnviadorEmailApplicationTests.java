dff058c9c671b9b1e85672793f6edf6f780578a7
piresalexsandro <allexsandro.pires@gmail.com>
2020-08-10 22:29:05 -0300

    Enviador de Email

---
+++ src/test/java/br/com/alphapires/enviadorEmail/enviadorEmail/EnviadorEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alphapires.enviadorEmail.enviadorEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviadorEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
